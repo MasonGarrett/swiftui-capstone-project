17f6c382952961bc125bce647897a1cfe0b6c3ae
Mason Garrett <thegarretts99@gmail.com>
2022-02-11 11:48:17 -0500

    - Rename ContentView to LaunchView - Setup Firebase with Pods - Add .gitignore

--- CapstoneProject/Views/LaunchView.swift
+++ CapstoneProject/Views/LaunchView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LaunchView.swift
 //  CapstoneProject
 //
 //  Created by Mason Garrett on 2022-02-11.
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LaunchView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LaunchView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LaunchView()
     }
 }
