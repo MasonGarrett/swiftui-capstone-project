4e2b14bc036478cc02bcf2c63bfba309f5fa6794
Mason Garrett <thegarretts99@gmail.com>
2022-02-11 11:37:15 -0500

    Initial Commit

---
+++ CapstoneProject/CapstoneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneProjectApp.swift
+//  CapstoneProject
+//
+//  Created by Mason Garrett on 2022-02-11.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CapstoneProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CapstoneProject
+//
+//  Created by Mason Garrett on 2022-02-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
